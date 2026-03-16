2b34b8dc54c3efd364bc75ebae707df1bd077211
CodeMaverick-143 <arpit.01.sarang.2005@gmail.com>
2026-03-16 17:55:14 +0530

    feat: Configure `STATIC_ROOT` and add production portal URLs to `CORS_ALLOWED_ORIGINS` and `CSRF_TRUSTED_ORIGINS`.

--- server/config/settings.py
+++ server/config/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files (User uploads)
 MEDIA_URL = "/media/"
@@ -163,11 +164,15 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "http://nstsdc-portal.nstsdc.org",
+    "https://nstsdc-portal.nstsdc.org",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://nstsdc-portal.nstsdc.org",
+    "https://nstsdc-portal.nstsdc.org",
 ]
 SESSION_COOKIE_SAMESITE = "Lax"
 CSRF_COOKIE_SAMESITE = "Lax"
